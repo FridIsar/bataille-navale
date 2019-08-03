fd64e8336fcef69428f1bb4f5b23bd7d96be81bc
Isar Fridriksson <isarfridriksson@gmail.com>
2019-08-03 15:30:02 +0200

    harmonization

--- Projet.java
+++ Projet.java
@@ -1,9 +1,9 @@
-public class Projet { //ajout
+public class Projet	{ //ajout
 
   public static final int L_CHAMP = 10;
   public static final int P_CHAMP = 1;
 
-  public static void main(String[] args) {
+  public static void main(String[] args)	{
     ChampBataille cb = new ChampBataille(L_CHAMP);
     System.out.println(cb);
     cb.setBoat(3,4);
