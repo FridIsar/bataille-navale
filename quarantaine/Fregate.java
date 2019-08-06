74845c5af3084e35f16b4baa875f0d1e96ba1eb6
isar fridriksson <isarfridriksson@gmail.com>
2019-08-06 16:46:16 +0200

    bateaux en quarantaine, ajout des positions

---
+++ quarantaine/Fregate.java
@@ -0,0 +1,70 @@
+public class Fregate implements Bateau	{
+	public static final int resistance = 6;
+	public static final int munitions = 10;
+	public static final int taille = 4;
+	public Orientation orientation =  Orientation.AntiDiagonal;
+
+	private int x;
+	private int y;
+
+	public Fregate()	{	}
+
+	public Fregate(int monX, int monY)	{
+		this.x = monX;
+		this.y = monY;
+	}
+
+	public int getX()	{
+		return this.x;
+	}
+
+	public int getY()	{
+		return this.y;
+	}
+
+	public Orientation getOrientation()	{
+		return this.orientation;
+	}
+
+	public void setRandomOrientation()	{
+		double nbAleatoire =  Math.random() * 4 + 1;
+		switch ((int) nbAleatoire)	{
+			case 1:
+					this.orientation = Orientation.Horizontal;
+					break;
+			case 2:
+					this.orientation = Orientation.Vertical;
+					break;
+			case 3:
+					this.orientation = Orientation.AntiDiagonal;
+					break;
+			case 4:
+					this.orientation = Orientation.Diagonal;
+					break;
+		}
+	}
+
+	public void setX(int monX)	{
+		this.x = monX;
+	}
+
+	public void setY(int monY)	{
+		this.y = monY;
+	}
+
+	public int getResistance()	{
+		return this.resistance;
+	}
+
+	public int getTaille()	{
+		return this.taille;
+	}
+
+	public boolean isInside()	{
+		return ((this.x >= 0 && this.x <= L_CHAMP) && (this.y >= 0 && this.y <= L_CHAMP));
+	}
+
+	public void avancer()	{
+		this.x += 1;
+	}
+}
